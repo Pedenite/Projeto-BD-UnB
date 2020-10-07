bbcafdcb52ad09d891a93b09e3fc0d93aeccf8e7
Pedenite <pedenite@gmail.com>
2020-10-07 16:59:34 -0300

    Adicionando scrips que populam as tabelas

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Projeto Final de BD 2020/1
 
 Ferramenta para auxiliar o professor no modelo de aulas à distância.
+
+## Banco
+*hostname:* `opessoa.com.br:3306`
+
+## Aplicação
+*host:* `api2.opessoa.com.br`
---
+++ documentacao/iniciaValores.sql
@@ -0,0 +1,10 @@
+INSERT INTO `aluno`
+(`matricula`,
+`nome`,
+`email`)
+VALUES
+('180021231', 'Kesley Kenny Guimarães Vasques', 'kesley.kenny@gmail.com'),
+('180021320', 'Keydson Estrela da Silva', 'gaydson@gmail.com'),
+('180022059', 'Letícia Dias', 'noobzinho@gmail.com'),
+('180026305', 'Pedro Henrique de Brito Agnes', 'pedenite@gmail.com'),
+('180026488', 'Pedro Pessoa Ramos', 'contato@opessoa.com.br');

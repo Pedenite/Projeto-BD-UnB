c11199cd21d3f13b91955dc3b22cd93c0880c73c
Pedenite <pedenite@gmail.com>
2020-11-08 13:58:02 -0300

    update

--- controllers/AlunosControllers.php
+++ controllers/AlunosControllers.php
@@ -14,7 +14,7 @@ class AlunosControllers {
     function getAlunos(){
         $retorno = $this->conn->query('SELECT * FROM aluno');
 
-        if($retorno){
+        if($retorno && $retorno->num_rows > 0){
 
             $arratAluno = array();
 
@@ -26,6 +26,41 @@ class AlunosControllers {
 
             return json_encode(array('status'=>'sucesso','dados'=>$arratAluno));
 
+        }else{
+
+            if($retorno && $retorno->num_rows == 0){
+
+                return json_encode(array('status'=>'sucesso','dados'=>array()));
+
+            }
+
+            return json_encode(array('status'=>'erro','dados'=>'Nenhum aluno retornado!'));
+
+        }
+
+    }
+
+    function insertAluno($matricula = '', $nome = '', $email = ''){ 
+        if($matricula == ''|| $nome == ''|| $email == ''){
+            return json_encode(array('status'=>'erro','dados'=>'Parametros insuficientes!'));
+        }
+        $retorno = $this->conn->query("INSERT INTO `aluno` (`matricula`,`nome`,`email`) VALUES ('".$this->conn->real_escape_string($matricula)."','".$this->conn->real_escape_string($nome)."','".$this->conn->real_escape_string($email)."');");
+        if($retorno){
+            return json_encode(array('status'=>'sucesso','dados'=>'Aluno '.$nome.' inserido na tabela.'));
+        }else{
+            return json_encode(array('status'=>'erro','dados'=>'Erro ao inserir aluno na tabela!'));
+        }
+    }
+
+    function deleteAluno($matricula = ''){ 
+        if($matricula == ''){
+            return json_encode(array('status'=>'erro','dados'=>'Parametros insuficientes!'));
+        }
+        $retorno = $this->conn->query("DELETE FROM `aluno` WHERE `matricula` = '".$this->conn->real_escape_string($matricula)."';");
+        if($retorno){
+            return json_encode(array('status'=>'sucesso','dados'=>'Aluno '.$matricula.' deletado da tabela.'));
+        }else{
+            return json_encode(array('status'=>'erro','dados'=>'Erro ao deletar aluno da tabela!'));
         }
     }
 }
